2995f7c92c217444c0a9ffba2c0e120beb127caf
Salsonh <sdeluca22@fed.idserve.net>
2022-11-17 12:38:52 +0100

    Project creation
    Creazione del progetto xcode

---
+++ Postepay-redesign/Postepay-redesign/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Postepay-redesign
+//
+//  Created by Salvatore De Luca on 17/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Postepay-redesign/Postepay-redesign/Postepay_redesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Postepay_redesignApp.swift
+//  Postepay-redesign
+//
+//  Created by Salvatore De Luca on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Postepay_redesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
