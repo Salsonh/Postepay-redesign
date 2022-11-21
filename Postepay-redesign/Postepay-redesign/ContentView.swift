dbda8a13fe8eef28cb2da5b78f89d495973da406
Salsonh <sdeluca22@fed.idserve.net>
2022-11-21 12:38:32 +0100

    Update ContentView.swift

--- Postepay-redesign/Postepay-redesign/ContentView.swift
+++ Postepay-redesign/Postepay-redesign/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showData: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                HStack {
+                    Text("SALDO CARTA")
+                        .foregroundColor(Color.secondary)
+                        .multilineTextAlignment(.leading)
+                        .lineLimit(0)
+                        .padding(0.0)
+                        
+                }
+                
+                Text(showData ? "2,50€" : "****")
+                
+                Button("Mostra") {
+                    showData.toggle()
+                    print(showData)
+                    
+                }
+                .foregroundColor(Color.secondary)
+
+                .padding(0.0)
+                
+                Spacer()
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("postepay").font(.headline).foregroundColor(Color.blue)
+                    
+                    
+                    
+                }
+                ToolbarItem(placement: .navigationBarTrailing){
+                    Image(systemName:"bell")
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +58,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
